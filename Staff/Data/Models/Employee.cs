79737a2e44c06d4c9a93864d599112788c35d328
Volodkovich Vladimir <fqaz2@mail.ru>
2021-07-08 14:43:48 +0300

    Add model "Employee"

---
+++ Staff/Data/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staff.Data.Models
+{
+    class Employee
+    {
+        [DisplayName("Id")]
+        [Browsable(false)]
+        public int Id { get; set; }
+        [DisplayName("Имя")]
+        public string Name { get; set; }
+        [DisplayName("Фамилия")]
+        public string Surname { get; set; }
+        [DisplayName("Отчество")]
+        public string Patronymic { get; set; }
+        [DisplayName("Должность")]
+        public string Position { get; set; }
+        [DisplayName("Дата рождения")]
+        public DateTime DateOfBirth { get; set; }
+        [DisplayName("Зарплата")]
+        public decimal Salary { get; set; }
+    }
+}
